2a903bbf11fe54bac8eec2ea8a452688646a79ad
romanovjke <romanov.kirill.biz@gmail.com>
2026-02-17 21:46:10 +0500

    Update container_manager.h

--- src/core/container_manager.h
+++ src/core/container_manager.h
@@ -1,3 +1,4 @@
+/*
 #ifndef CONTAINER_MANAGER_H
 #define CONTAINER_MANAGER_H
 
@@ -24,6 +25,42 @@ private:
     void generateRandomSalt(uint8_t* buffer, int length);
     
     // Вспомогательная функция для заполнения заголовка по умолчанию
+    SFMHeader createDefaultHeader();
+};
+
+#endif
+*/
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------17.02.2026
+#ifndef CONTAINER_MANAGER_H
+#define CONTAINER_MANAGER_H
+
+#include <string>
+#include <vector>
+#include "../format/sfm_header.h" // Убедитесь, что путь верный относительно папки core
+
+class ContainerManager {
+public:
+    ContainerManager();
+
+    // Создание пустого контейнера (существующий метод)
+    bool createContainer(const std::string& filePath, const std::string& password, long sizeInBytes);
+
+    // Проверка пароля (существующий метод)
+    bool openContainer(const std::string& filePath, const std::string& password);
+
+    // --- НОВЫЕ МЕТОДЫ (Интеграция prototype_aess.cpp) ---
+    
+    // Шифрует обычный файл inputPath в формат SFM по пути outputPath
+    bool encryptFile(const std::string& inputPath, const std::string& outputPath, const std::string& password);
+
+    // Расшифровывает SFM-файл inputPath в обычный файл outputPath
+    bool decryptFile(const std::string& inputPath, const std::string& outputPath, const std::string& password);
+
+private:
+    void generateRandomSalt(uint8_t* buffer, int length);
     SFMHeader createDefaultHeader();
 };
 
