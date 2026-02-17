2d686d91ff199dcc75be084ef5a2897daa247fc3
romanovjke <romanov.kirill.biz@gmail.com>
2026-02-17 21:45:43 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 #include "core/container_manager.h"
@@ -37,6 +38,72 @@ int main(int argc, char* argv[]) {
         } else {
             std::cout << "Failed to open container." << std::endl;
         }
+    }
+    else {
+        std::cout << "Unknown command." << std::endl;
+    }
+
+    return 0;
+}
+*/
+
+
+//--------------------------------------------------------------------------------------------------------------------------17.02.2026
+
+#include <iostream>
+#include <string>
+#include "core/container_manager.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage: sfm_tool <command> <args...>\n";
+        std::cout << "Commands:\n";
+        std::cout << "  create <vault_name> [size_mb]   Create a new empty vault\n";
+        std::cout << "  open   <vault_name>             Check vault password\n";
+        std::cout << "  enc    <input_file> <out_file>  Encrypt a single file\n";
+        std::cout << "  dec    <sfm_file>   <out_file>  Decrypt a single file\n";
+        return 1;
+    }
+
+    std::string command = argv[1];
+    
+    std::string password;
+    std::cout << "Enter Password: ";
+    std::cin >> password;
+
+    ContainerManager manager;
+
+    if (command == "create") {
+        std::string filePath = argv[2];
+        long sizeMb = (argc >= 4) ? std::stol(argv[3]) : 10;
+        long sizeBytes = sizeMb * 1024 * 1024;
+        
+        if (manager.createContainer(filePath, password, sizeBytes)) {
+            std::cout << "Container created!" << std::endl;
+        }
+    } 
+    else if (command == "open") {
+        std::string filePath = argv[2];
+        manager.openContainer(filePath, password);
+    }
+
+    else if (command == "enc") {
+        if (argc < 4) {
+            std::cout << "Usage: sfm_tool enc <input> <output>" << std::endl;
+            return 1;
+        }
+        std::string input = argv[2];
+        std::string output = argv[3];
+        manager.encryptFile(input, output, password);
+    }
+    else if (command == "dec") {
+        if (argc < 4) {
+            std::cout << "Usage: sfm_tool dec <input> <output>" << std::endl;
+            return 1;
+        }
+        std::string input = argv[2];
+        std::string output = argv[3];
+        manager.decryptFile(input, output, password);
     }
     else {
         std::cout << "Unknown command." << std::endl;
