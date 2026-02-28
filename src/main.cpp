389602d80e830de6269fcf6fe02a8d8147fe624f
romanovjke <romanov.kirill.biz@gmail.com>
2026-02-28 21:29:29 +0500

    try new "del" function - delete

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,3 @@
-/*
-#include <iostream>
-#include <string>
-#include "core/container_manager.h"
-
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cout << "Usage: sfm_tool <command> <file> [size_mb]\n";
-        std::cout << "Examples:\n";
-        std::cout << "  ./sfm_tool create my_vault.sfm 20\n";
-        std::cout << "  ./sfm_tool open my_vault.sfm\n";
-        return 1;
-    }
-
-    std::string command = argv[1];
-    std::string filePath = argv[2];
-
-    std::string password;
-    std::cout << "Enter Password: ";
-    std::cin >> password;
-
-    ContainerManager manager;
-
-    if (command == "create") {
-        long sizeMb = (argc >= 4) ? std::stol(argv[3]) : 10;
-        long sizeBytes = sizeMb * 1024 * 1024;
-        
-        if (manager.createContainer(filePath, password, sizeBytes)) {
-            std::cout << "Great success! High five!" << std::endl;
-        } else {
-            std::cout << "Error creating container." << std::endl;
-        }
-    } 
-    else if (command == "open") {  // <--- THIS WAS MISSING
-        if (manager.openContainer(filePath, password)) {
-            // Success message is handled inside openContainer, 
-            // but we can add one here too if we want.
-        } else {
-            std::cout << "Failed to open container." << std::endl;
-        }
-    }
-    else {
-        std::cout << "Unknown command." << std::endl;
-    }
-
-    return 0;
-}
-*/
-
-
-//--------------------------------------------------------------------------------------------------------------------------17.02.2026
-
 #include <iostream>
 #include <string>
 #include "core/container_manager.h"
@@ -62,6 +10,7 @@ int main(int argc, char* argv[]) {
         std::cout << "  open   <vault_name>             Check vault password\n";
         std::cout << "  enc    <input_file> <out_file>  Encrypt a single file\n";
         std::cout << "  dec    <sfm_file>   <out_file>  Decrypt a single file\n";
+        std::cout << "  del    <file_path>              Securely wipe & delete a file\n";
         return 1;
     }
 
@@ -104,6 +53,18 @@ int main(int argc, char* argv[]) {
         std::string input = argv[2];
         std::string output = argv[3];
         manager.decryptFile(input, output, password);
+    }
+    else if (command == "del") {
+        std::string filePath = argv[2];
+        std::cout << "WARNING: This will permanently destroy data in: " << filePath << std::endl;
+        std::cout << "Are you sure? (y/n): ";
+        char confirm;
+        std::cin >> confirm;
+        if (confirm == 'y' || confirm == 'Y') {
+            manager.secureDeleteFile(filePath);
+        } else {
+            std::cout << "Operation cancelled." << std::endl;
+        }
     }
     else {
         std::cout << "Unknown command." << std::endl;
